7ba0208156f01e4c925d9d45ca28c3ad32769949
SBUSTO <sbj2001@outlook.com>
2024-07-27 18:58:48 -0400

    modified JS code with correct img srcs in the agent array, and correct img src in the .setAttribute('src', ...)

--- client/js/at work.js
+++ client/js/at work.js
@@ -1,23 +1,23 @@
 const agents = [
-    { name: "Astra", img: "/images/Astra_icon.webp" },
-    { name: "Breach", img: "/images/breach_icon.webp" },
-    { name: "Brimstone", img: "/images/brimstone_icon.webp" },
-    { name: "Chamber", img: "/images/chamber_icon.webp" },
-    { name: "Clove", img: "/images/clove_icon.webp" },
-    { name: "Cypher", img: "/images/cypher_icon.webp" },
-    { name: "Jett", img: "/images/jett_icon.webp" },
-    { name: "KAY/O", img: "/images/kayo_icon.webp" },
-    { name: "Killjoy", img: "/images/killjoy_icon.webp" },
-    { name: "Neon", img: "/images/neon_icon.webp" },
-    { name: "Omen", img: "/images/omen_icon.webp" },
-    { name: "Phoenix", img: "/images/phoenix_icon.webp" },
-    { name: "Raze", img: "/images/raze_icon.webp" },
-    { name: "Reyna", img: "/images/reyna_icon.webp" },
-    { name: "Sage", img: "/images/sage_icon.webp" },
-    { name: "Skye", img: "/images/skye_icon.webp" },
-    { name: "Sova", img: "/images/sova_icon.webp" },
-    { name: "Viper", img: "/images/viper_icon.webp" },
-    { name: "Yoru", img: "/images/yoru_icon.webp" },
+    { name: "Astra", img: "/client/images/Astra_icon.webp" },
+    { name: "Breach", img: "/client/images/breach_icon.webp" },
+    { name: "Brimstone", img: "/client/images/brimstone_icon.webp" },
+    { name: "Chamber", img: "/client/images/chamber_icon.webp" },
+    { name: "Clove", img: "/client/images/clove_icon.webp" },
+    { name: "Cypher", img: "/client/images/cypher_icon.webp" },
+    { name: "Jett", img: "/client/images/jett_icon.webp" },
+    { name: "KAY/O", img: "/client/images/kayo_icon.webp" },
+    { name: "Killjoy", img: "/client/images/killjoy_icon.webp" },
+    { name: "Neon", img: "/client/images/neon_icon.webp" },
+    { name: "Omen", img: "/client/images/omen_icon.webp" },
+    { name: "Phoenix", img: "/client/images/phoenix_icon.webp" },
+    { name: "Raze", img: "/client/images/raze_icon.webp" },
+    { name: "Reyna", img: "/client/images/reyna_icon.webp" },
+    { name: "Sage", img: "/client/images/sage_icon.webp" },
+    { name: "Skye", img: "/client/images/skye_icon.webp" },
+    { name: "Sova", img: "/client/images/sova_icon.webp" },
+    { name: "Viper", img: "/client/images/viper_icon.webp" },
+    { name: "Yoru", img: "/client/images/yoru_icon.webp" },
 ];
 
 const selected = [];
@@ -62,7 +62,7 @@ randomButton.addEventListener('click', () => {
     // update contents of existing element
 
     const agentImg = document.getElementById('agent-img');
-    agentImg.setAttribute('src', "selectedAgent"); // replace selectedAgent with correct image src
+    agentImg.setAttribute('src', agent.img); // replace selectedAgent with correct image src
     
     const agentName = document.getElementById('agent-name');
     agentName.innerText = agent.name;
